72280c3c3d017491facc9913f90603610e16f3dd
Sparsh Rawat <92641998+Sparsh225@users.noreply.github.com>
2023-05-25 19:37:00 +0530

    traversal added

--- DoublyLinkedList.java
+++ DoublyLinkedList.java
@@ -1,10 +1,81 @@
 package technical;
 
 public class DoublyLinkedList {
+	private Listnode head;
+	private Listnode tail;
+	private int length;
+
+	private class Listnode {
+		private Listnode pre;
+		private Listnode next;
+		private int data;
+
+		public Listnode(int data) {
+			this.data = data;
+			this.next = null;
+			this.pre = null;
+		}
+	}
+
+	public DoublyLinkedList() {
+		this.head = null;
+		this.tail = null;
+		this.length = 0;
+	}
+
+	public boolean isEmpty() {
+		return length == 0;
+	}
+
+	public int length() {
+		return length;
+	}
+
+	public void insertatlast(int value) {
+		Listnode newnode = new Listnode(value);
+		if (head == null) {
+			head = newnode;
+		} else {
+			tail.next = newnode;
+		}
+		newnode.pre = tail;
+		tail = newnode;
+		length++;
+	}
+
+	public void displayforward() {
+		if (head == null) {
+			return;
+		}
+		Listnode temp = head;
+		while (temp != null) {
+			System.out.print(temp.data + "-->");
+			temp = temp.next;
+		}
+		System.out.print("null");
+	}
+
+	public void displaybackward() {
+		if (tail == null) {
+			return;
+		}
+		Listnode temp = tail;
+		while (temp != null) {
+			System.out.print(temp.data + "-->");
+			temp = temp.pre;
+		}
+		System.out.print("null");
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		DoublyLinkedList dll = new DoublyLinkedList();
+		dll.insertatlast(1);
+		dll.insertatlast(13);
+		dll.insertatlast(23);
+		dll.insertatlast(55);
+		dll.insertatlast(63);
+		dll.displayforward();System.out.println();
+		dll.displaybackward();
 	}
 
 }
