0f6295817cb9f1ca4303fc529aa96eac12049518
Sparsh Rawat <92641998+Sparsh225@users.noreply.github.com>
2023-05-26 13:39:15 +0530

    initial commit

---
+++ CircularLinkedList.java
@@ -0,0 +1,49 @@
+package technical;
+
+public class CircularLinkedList {
+
+	private Listnode last;
+	private int length;
+
+	public class Listnode {
+		private Listnode next;
+		private int data;
+
+		public Listnode(int data) {
+			this.data = data;
+		}
+	}
+
+	public CircularLinkedList() {
+		last = null;
+		length = 0;
+	}
+
+	public int length() {
+		return length;
+	}
+
+	public boolean isEmpty() {
+		return length == 0;
+	}
+
+	public void createcircularlist() {
+		Listnode first = new Listnode(1);
+		Listnode second = new Listnode(23);
+		Listnode third = new Listnode(55);
+		Listnode fourth = new Listnode(66);
+		first.next = second;
+		second.next = third;
+		third.next = fourth;
+		fourth.next = first;
+
+		last = fourth;
+
+	}
+
+	public static void main(String[] args) {
+		CircularLinkedList csl = new CircularLinkedList();
+		csl.createcircularlist();
+	}
+
+}
