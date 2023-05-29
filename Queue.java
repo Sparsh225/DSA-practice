7049d7a9248c76f41d72ed3913a81089f2013c1c
Sparsh Rawat <92641998+Sparsh225@users.noreply.github.com>
2023-05-29 19:36:03 +0530

    generatebinarynumber

--- Queue.java
+++ Queue.java
@@ -65,6 +65,20 @@ public class Queue {
 		return result;
 	}
 	
+	String [] generatebinarynumber(int n) {
+		Queue<String> q=new LinkedList<>();
+		String [] result=new String[n];
+		q.offer("1");
+		for(int i=0;i<n;i++) {
+			result[i]=q.poll();
+			String n1=result[i]+"0";
+			String n2=result[i]+"1";
+			q.offer(n1);
+			q.offer(n2);
+		}
+		return result;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Queue q = new Queue();
