295f559ac6f3d7778a008e1d1740ae91bcf2e46a
Sparsh Rawat <92641998+Sparsh225@users.noreply.github.com>
2023-05-31 20:26:03 +0530

    Update BinarySearchTree.java

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,9 +1,30 @@
 package technical;
 
 public class BinarySearchTree {
-
+	private Treenode root;
+    public class Treenode{
+    	private Treenode left;
+    	private Treenode right;
+    	private int data;
+    	public Treenode(int data) {
+    		this.data=data;
+    	}
+    }
+    
+    public Treenode insert(Treenode root,int data) {
+    	if(root==null) {
+    		root=new Treenode(data);
+    		return root;
+    	}
+    	if(data < root.data) {
+    		root.left=insert(root.left,data);
+    	}else {
+    		root.right=insert(root.right,data);
+    	}
+    	return root;
+    }
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 
 	}
 
