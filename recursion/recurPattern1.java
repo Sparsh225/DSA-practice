544f2897f723629f0fec37acf5884cdbf99293be
Sparsh Rawat <92641998+Sparsh225@users.noreply.github.com>
2023-06-07 11:32:57 +0530

    Add files via upload

---
+++ recursion/recurPattern1.java
@@ -0,0 +1,27 @@
+package technical;
+
+public class recurPattern1 {
+
+	static void printstar(int noofstar) {
+		if (noofstar == 0) {
+			return;
+		}
+		System.out.print("*");
+		printstar(noofstar - 1);
+	}
+
+	static void printpattern(int rows, int cuurentrow) {
+		if (rows == 0) {
+			return;
+		}
+		printstar(cuurentrow);// print row
+		System.out.println();// move into new line
+		printpattern(rows - 1, cuurentrow + 1);
+	}
+
+	public static void main(String[] args) {
+		printpattern(5, 1);
+
+	}
+
+}
