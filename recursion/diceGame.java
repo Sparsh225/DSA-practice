6b0c80e68eaae5bf1b1cdeb247b9191d4037283e
Sparsh Rawat <92641998+Sparsh225@users.noreply.github.com>
2023-06-08 20:47:56 +0530

    Add files via upload

---
+++ recursion/diceGame.java
@@ -0,0 +1,34 @@
+package technical;
+
+import java.util.ArrayList;
+
+public class diceGame {
+
+	public static ArrayList<String> gamedice(int current, int target) {
+		if (current == target) {
+			ArrayList<String> list = new ArrayList<>();
+			list.add("");
+			return list;
+		}
+		if (current > target) {
+			ArrayList<String> list = new ArrayList<>();
+			return list;
+		}
+		ArrayList<String> result = new ArrayList<>();
+		for (int i = 1; i <= 6; i++) {
+			int newval = current + i;
+			ArrayList<String> temp = gamedice(newval, target);
+			for (String s : temp) {
+				result.add(s + i);
+			}
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		ArrayList<String> res = gamedice(0, 10);
+		System.out.println(res);
+		System.out.println(res.size());
+	}
+
+}
