0da2c1a372b487d80ec4c92a1d46eedac426417c
Sparsh Rawat <92641998+Sparsh225@users.noreply.github.com>
2023-06-14 10:32:56 +0530

    Add files via upload

---
+++ recursion/JosephusProblem.java
@@ -0,0 +1,18 @@
+package technical;
+
+public class JosephusProblem {
+
+	public static int josephus(int n, int k) {
+		if (n == 1) {
+			return 1;
+		} else {
+			return (josephus(n - 1, k) + k - 1) % n + 1;
+		}
+	}
+
+	public static void main(String[] args) {
+
+		System.out.print(josephus(7, 1));
+	}
+
+}
