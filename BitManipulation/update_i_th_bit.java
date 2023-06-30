a26fea256f0668a5531dd3715d522fdf1e66571d
Sparsh Rawat <92641998+Sparsh225@users.noreply.github.com>
2023-06-30 10:26:48 +0530

    Add files via upload

---
+++ BitManipulation/update_i_th_bit.java
@@ -0,0 +1,16 @@
+package bitManipulation;
+
+public class update_i_th_bit {
+
+	public static void main(String[] args) {
+		int n = 22;
+		int i = 2;
+		int b = 0;
+		int mask = b << i;
+		int newnum = (n | mask);
+		int mask2 = b << i;
+		System.out.print(newnum | mask2);
+
+	}
+
+}
