c087f05d9c3c1805b94457995759a768d7d260f6
Sparsh Rawat <92641998+Sparsh225@users.noreply.github.com>
2023-07-02 19:44:01 +0530

    Add files via upload

---
+++ Arrays/smallernumberarry.java
@@ -0,0 +1,27 @@
+package technical;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class smallernumberarry {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a = { 8, 1, 2, 2, 3 };
+		Arrays.sort(a);
+		HashMap<Integer, Integer> map = new HashMap<>();
+		for (int i = 0; i < a.length; i++) {
+			map.put(a[i], i);
+
+		}
+		int[] out = new int[a.length];
+		for (int i = 0; i < a.length; i++) {
+			out[i] = map.get(a[i]);
+		}
+		for (int i = 0; i < a.length; i++) {
+			System.out.print(out[i] + " ");
+		}
+	}
+
+}
