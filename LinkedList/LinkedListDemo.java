52b96fe327a138a7306d9374087c8f78e7b1ff8f
Sparsh Rawat <92641998+Sparsh225@users.noreply.github.com>
2023-07-07 18:42:17 +0530

    Add files via upload

---
+++ LinkedList/LinkedListDemo.java
@@ -0,0 +1,19 @@
+package LinkedList;
+
+/*class Node{
+	private int data;
+	private Node next;
+	Node(int data){
+		this.next=next;
+		this.next=null;
+	}
+}
+*/
+public class LinkedListDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
